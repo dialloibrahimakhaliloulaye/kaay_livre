38c33d84df597e249f651758dce46967fdb7fdad
aliouneniome <aliouneniome21@gmail.com>
2022-06-11 02:18:50 +0000

    integration google maps

--- lib/authentification/login_screen.dart
+++ lib/authentification/login_screen.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import  'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
@@ -14,24 +14,19 @@ class LoginScreen extends StatefulWidget
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
-
 class _LoginScreenState extends State<LoginScreen>
 {
   TextEditingController emailTextEditingController = TextEditingController();
   TextEditingController passwordTextEditingController = TextEditingController();
 
-  validateForm()
-  {
-    if(!emailTextEditingController.text.contains("@"))
-    {
+  validateForm() {
+    if (!emailTextEditingController.text.contains("@")) {
       Fluttertoast.showToast(msg: "Email address is not valid.");
     }
-    else if(passwordTextEditingController.text.isEmpty)
-    {
-      Fluttertoast.showToast(msg: "Password is require.");
+    else if (passwordTextEditingController.text.isEmpty) {
+      Fluttertoast.showToast(msg: "Password is required.");
     }
-    else
-    {
+    else {
       loginDriverNow();
     }
   }
@@ -41,8 +36,7 @@ class _LoginScreenState extends State<LoginScreen>
     showDialog(
         context: context,
         barrierDismissible: false,
-        builder: (BuildContext c)
-        {
+        builder: (BuildContext c) {
           return ProgressDialog(message: "Processing, Please wait...",);
         }
     );
@@ -51,162 +45,152 @@ class _LoginScreenState extends State<LoginScreen>
         await fAuth.signInWithEmailAndPassword(
           email: emailTextEditingController.text.trim(),
           password: passwordTextEditingController.text.trim(),
-        ).catchError((msg){
+        ).catchError((msg) {
           Navigator.pop(context);
           Fluttertoast.showToast(msg: "Error: " + msg.toString());
         })
     ).user;
 
-    if(firebaseUser != null)
-    {
-
-      currentFirebaseUser = firebaseUser;
-      Fluttertoast.showToast(msg: "Login Successful.");
-      Navigator.push(context, MaterialPageRoute(builder: (C)=> const MySplashScreen()));
-
-      DatabaseReference driversRef = FirebaseDatabase.instance.ref().child("users");
-      driversRef.child(firebaseUser.uid).once().then((driverKey)
-      {
+    if (firebaseUser != null) {
+      DatabaseReference driversRef = FirebaseDatabase.instance.ref().child(
+          "drivers");
+      driversRef.child(firebaseUser.uid).once().then((driverKey) {
         final snap = driverKey.snapshot;
-        if(snap.value != null)
-        {
+        if (snap.value != null) {
           currentFirebaseUser = firebaseUser;
           Fluttertoast.showToast(msg: "Login Successful.");
-          Navigator.push(context, MaterialPageRoute(builder: (c)=> const MySplashScreen()));
+          Navigator.push(context,
+              MaterialPageRoute(builder: (C) => const MySplashScreen()));
         }
-        else
-        {
+        else {
           Fluttertoast.showToast(msg: "No record exist with this email.");
           fAuth.signOut();
-          Navigator.push(context, MaterialPageRoute(builder: (c)=> const MySplashScreen()));
+          Navigator.push(context,
+              MaterialPageRoute(builder: (c) => const MySplashScreen()));
         }
       });
     }
-    else
-    {
+    else {
       Navigator.pop(context);
       Fluttertoast.showToast(msg: "Error Occurred during Login.");
     }
-  }
 
 
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        backgroundColor: Colors.black,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            children: [
+                const SizedBox(height: 30,),
 
-              const SizedBox(height: 30,),
-
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Image.asset("images/logo1.png"),
-              ),
-
-              const SizedBox(height: 10,),
-
-              const Text(
-                "Login as a Driver",
-                style: TextStyle(
-                  fontSize: 26,
-                  color: Colors.grey,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-
-
-
-              TextField(
-                controller: emailTextEditingController,
-                keyboardType: TextInputType.emailAddress,
-                style: const TextStyle(
-                  color: Colors.grey,
-                ),
-                decoration: const InputDecoration(
-                  labelText: "Email",
-                  hintText: "Email",
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey),
-                  ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey),
-                  ),
-                  hintStyle: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 10,
-                  ),
-                  labelStyle: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 14,
-                  ),
-                ),
-              ),
-
-              TextField(
-                controller: passwordTextEditingController,
-                keyboardType: TextInputType.text,
-                obscureText: true,
-                style: const TextStyle(
-                  color: Colors.grey,
-                ),
-                decoration: const InputDecoration(
-                  labelText: "Password",
-                  hintText: "Password",
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey),
-                  ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.grey),
-                  ),
-                  hintStyle: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 10,
-                  ),
-                  labelStyle: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 14,
-                  ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Image.asset("images/logo1.png"),
                 ),
 
-              ),
-              const SizedBox(height: 20,),
+                const SizedBox(height: 10,),
 
-              ElevatedButton(
-                onPressed: ()
-                {
-                  validateForm();
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.lightGreenAccent,
-                ),
-                child: const Text(
-                  "Login",
+                const Text(
+                  "Login as a Driver",
                   style: TextStyle(
-                    color: Colors.black54,
-                    fontSize: 18,
+                    fontSize: 26,
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
 
-              TextButton(
-                child: const Text(
-                  "Do not Have an Account? SignUp Here",
-                  style: TextStyle(color: Colors.grey),
+
+                TextField(
+                  controller: emailTextEditingController,
+                  keyboardType: TextInputType.emailAddress,
+                  style: const TextStyle(
+                    color: Colors.grey,
+                  ),
+                  decoration: const InputDecoration(
+                    labelText: "Email",
+                    hintText: "Email",
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 10,
+                    ),
+                    labelStyle: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 14,
+                    ),
+                  ),
                 ),
-                onPressed: ()
-                {
-                  Navigator.push(context, MaterialPageRoute(builder: (c)=> SignUpScreen()));
-                },
-              ),
-            ],
+
+                TextField(
+                  controller: passwordTextEditingController,
+                  keyboardType: TextInputType.text,
+                  obscureText: true,
+                  style: const TextStyle(
+                    color: Colors.grey,
+                  ),
+                  decoration: const InputDecoration(
+                    labelText: "Password",
+                    hintText: "Password",
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 10,
+                    ),
+                    labelStyle: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 14,
+                    ),
+                  ),
+
+                ),
+                const SizedBox(height: 20,),
+
+                ElevatedButton(
+                  onPressed: () {
+                    validateForm();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.lightGreenAccent,
+                  ),
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+
+                TextButton(
+                  child: const Text(
+                    "Do not Have an Account? SignUp Here",
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (c) => SignUpScreen()));
+                  },
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    }
   }
 }
--- lib/global/global.dart
+++ lib/global/global.dart
@@ -1,4 +1,3 @@
 import 'package:firebase_auth/firebase_auth.dart';
-
 final FirebaseAuth fAuth = FirebaseAuth.instance;
 User? currentFirebaseUser;
