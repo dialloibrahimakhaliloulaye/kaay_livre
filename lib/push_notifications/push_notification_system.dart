31598254596a65b304ff7014b53efec422afc680
aliouneniome <aliouneniome21@gmail.com>
2022-06-29 11:28:59 +0000

    send user rideRequestId in Push Notifications to Driver

--- lib/push_notifications/push_notification_system.dart
+++ lib/push_notifications/push_notification_system.dart
@@ -14,6 +14,8 @@ class PushNotificationSystem
     {
       if(remoteMessage != null)
       {
+        print("This is Ride Request Id :: ");
+        print(remoteMessage.data["rideRequestId"]);
         //display ride request information - user information who request a ride
       }
 
@@ -24,6 +26,8 @@ class PushNotificationSystem
     //When the app is open and it receives a push notification
     FirebaseMessaging.onMessage.listen((RemoteMessage? remoteMessage)
     {
+      print("This is Ride Request Id :: ");
+      print(remoteMessage!.data["rideRequestId"]);
       //display ride request information - user information who request a ride
     });
 
@@ -31,6 +35,8 @@ class PushNotificationSystem
     //when the app is in the background and opened directly from the push notification
     FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage? remoteMessage)
     {
+      print("This is Ride Request Id :: ");
+      print(remoteMessage!.data["rideRequestId"]);
       //display ride request information - user information who request a ride
     });
 
@@ -48,7 +54,7 @@ class PushNotificationSystem
         .child("Token")
         .set(registrationToken);
 
-    messaging.subscribeToTopic("AllDrivers");
-    messaging.subscribeToTopic("Allusers");
+    messaging.subscribeToTopic("allDrivers");
+    messaging.subscribeToTopic("allUsers");
   }
 }
