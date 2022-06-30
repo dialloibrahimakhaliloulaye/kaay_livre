aa4939a6f5985480f12cf27e1fc75e29847f5c81
aliouneniome <aliouneniome21@gmail.com>
2022-06-30 14:57:55 +0000

    save assigned driver details to user RideRequest Information

--- lib/global/global.dart
+++ lib/global/global.dart
@@ -5,9 +5,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:kaay_livre/models/user_model.dart';
 
+import '../models/driver_data.dart';
+
 
 final FirebaseAuth fAuth = FirebaseAuth.instance;
 User? currentFirebaseUser;
 UserModel? userModelCurrentInfo;
 StreamSubscription<Position>? streamSubscriptionPosition;
 AssetsAudioPlayer audioPlayer = AssetsAudioPlayer();
+Position? driverCurrentPosition;
+DriverData onlineDriverData = DriverData();
--- lib/tabPages/home_tab.dart
+++ lib/tabPages/home_tab.dart
@@ -33,7 +33,7 @@ class _HomeTabPageState extends State<HomeTabPage>
     zoom: 14.4746,
   );
 
-  Position? driverCurrentPosition;
+
   var geoLocator = Geolocator();
   LocationPermission? _locationPermission;
 
@@ -72,6 +72,29 @@ class _HomeTabPageState extends State<HomeTabPage>
   readCurrentDriverInformation() async
   {
     currentFirebaseUser = fAuth.currentUser;
+    await FirebaseDatabase.instance.ref()
+        .child("drivers")
+        .child(currentFirebaseUser!.uid)
+        .once()
+        .then((DatabaseEvent snap)
+    {
+      if(snap.snapshot.value != null)
+      {
+        onlineDriverData.id = (snap.snapshot.value as Map)["id"];
+        onlineDriverData.name = (snap.snapshot.value as Map)["name"];
+        onlineDriverData.phone = (snap.snapshot.value as Map)["phone"];
+        onlineDriverData.email = (snap.snapshot.value as Map)["email"];
+        onlineDriverData.car_color = (snap.snapshot.value as Map)["car_details"]["car_color"];
+        onlineDriverData.car_model = (snap.snapshot.value as Map)["car_details"]["car_model"];
+        onlineDriverData.car_number = (snap.snapshot.value as Map)["car_details"]["car_number"];
+
+        print("Car Details :: ");
+        print(onlineDriverData.car_color);
+        print(onlineDriverData.car_model);
+        print(onlineDriverData.car_number);
+      }
+    });
+
     PushNotificationSystem pushNotificationSystem = PushNotificationSystem();
     pushNotificationSystem.initializeCloudMessaging(context);
     pushNotificationSystem.generateAndGetToken();
